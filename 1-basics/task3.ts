88cad4965ae626ee1f5954bb07cd0e18bbe4127b
Jevgenijs Kozlovs <jevgenijs_kozlovs@epam.com>
2022-12-18 18:16:35 +0200

    feat: task3 stub

---
+++ 1-basics/task3.ts
@@ -0,0 +1,4 @@
+// Task 3 asks to use Babel, to transpile previous tasks to ES6.
+// However you've suggested to use TypeScript if possible, and I'm already using
+// ES6 modules by default. So I'm not sure what should I transpile in these tasks.
+// I bealive it's ok to skip this part.
