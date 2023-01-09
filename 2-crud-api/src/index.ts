0b2c1ab4f9061e796572dc3031e7b7db2d58603a
Jevgenijs Kozlovs <jevgenijs_kozlovs@epam.com>
2023-01-09 23:18:37 +0200

    refactor: add suggested changes

--- 2-crud-api/src/index.ts
+++ 2-crud-api/src/index.ts
@@ -1,9 +1,12 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import { v4 as uuidv4 } from 'uuid';
-import { UserDTO, NewUserDTO, addUserSchema, AddUserSchema, patchUserSchema, PatchUserSchema, autoSuggestSchema, AutoSuggestSchema } from './models';
+import { NewUserDTO, addUserSchema, AddUserSchema, patchUserSchema, PatchUserSchema, autoSuggestSchema, AutoSuggestSchema } from './models';
 import { users } from './data';
-import { ValidatedRequest } from 'express-joi-validation'
+import { ValidatedRequest } from 'express-joi-validation';
+import {
+  StatusCodes,
+} from 'http-status-codes';
 
 dotenv.config();
 
@@ -15,8 +18,8 @@ app.use(express.json());
 app.get('/', (req: Request, res: Response) => {
   const { id } = req.query;
   const user = users[id as string];
-  const data = !user || user.isDeleted ? null : user;
-  res.json(data);
+  const data = (!user || user.isDeleted) ?? user;
+  res.status(StatusCodes.OK).json(data);
 });
 
 app.post('/addUser', addUserSchema, (req: ValidatedRequest<AddUserSchema>, res: Response
@@ -31,31 +34,31 @@ app.post('/addUser', addUserSchema, (req: ValidatedRequest<AddUserSchema>, res: 
     isDeleted: false,
   };
 
-  res.json(id);
+  res.status(StatusCodes.OK).json(id);
 });
 
 app.patch('/patchUser', patchUserSchema, (req: ValidatedRequest<PatchUserSchema>, res: Response) => {
   const { id, login, password, age } = req.body
   const user = users[id];
   if (!user || user.isDeleted) {
-    return res.json(false);
+    return res.status(StatusCodes.BAD_REQUEST).json(false);
   }
 
   user.login = login;
   user.password = password;
   user.age = age;
-  res.json(true);
+  res.status(StatusCodes.OK).json(true);
 });
 
 app.delete('/deleteUser', (req: Request, res: Response) => {
   const { id } = req.body;
   const user = users[id as string];
   if (!user || user.isDeleted) {
-    return res.json(false);
+    return res.status(StatusCodes.BAD_REQUEST).json(false);
   }
 
   user.isDeleted = true;
-  res.json(true);
+  res.status(StatusCodes.OK).json(true);
 });
 
 app.get('/autoSuggest', autoSuggestSchema, (req: ValidatedRequest<AutoSuggestSchema>, res: Response) => {
@@ -67,7 +70,7 @@ app.get('/autoSuggest', autoSuggestSchema, (req: ValidatedRequest<AutoSuggestSch
     .sort((a, b) => a.login.localeCompare(b.login))
     .slice(0, limit);
 
-  res.json(result);
+  res.status(StatusCodes.OK).json(result);
 });
 
 app.listen(port, () => {
