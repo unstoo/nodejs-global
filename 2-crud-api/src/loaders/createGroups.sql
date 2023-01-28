fcd39a767b44c98c678e3ae92a3fc1f2c68a5891
Jevgenijs Kozlovs <unstoo@gmail.com>
2023-01-28 20:32:03 +0200

    feat: add user_group table

--- 2-crud-api/src/loaders/createGroups.sql
+++ 2-crud-api/src/loaders/createGroups.sql
@@ -1,18 +1,33 @@
-
 DROP TABLE IF EXISTS groups;
 
+CREATE TABLE groups (
+  id uuid primary key DEFAULT uuid_generate_v4(),
+  name VARCHAR(128) NOT NULL,
+  premissions text [] NOT NULL
+);
 
-CREATE TABLE groups (id uuid primary key DEFAULT uuid_generate_v4(),
-                                                 name VARCHAR(128) NOT NULL,
-                                                                   premissions text[] NOT NULL);
-
-
-INSERT INTO groups (name, premissions)
-VALUES ('admin_group', ARRAY['READ',
+INSERT INTO
+  groups (name, premissions)
+VALUES
+  (
+    'admin_group',
+    ARRAY ['READ',
                              'WRITE',
                              'DELETE',
                              'SHARE',
-                             'UPLOAD_FILES']), ('user_group', ARRAY['READ',
+                             'UPLOAD_FILES']
+  ),
+  (
+    'user_group',
+    ARRAY ['READ',
                                                                     'WRITE',
-                                                                    'SHARE']);
+                                                                    'SHARE']
+  );
 
+DROP TABLE IF EXISTS user_group;
+
+CREATE TABLE user_group (
+  user_id uuid REFERENCES users (id) ON DELETE CASCADE,
+  group_id uuid REFERENCES groups (id) ON DELETE CASCADE,
+  CONSTRAINT user_group_pkey PRIMARY KEY (user_id, group_id)
+);
