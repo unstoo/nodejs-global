68544d882540a0983a18d350c12cb9a7e36d828e
Jevgenijs Kozlovs <unstoo@gmail.com>
2023-02-13 00:40:08 +0200

    feat: add error handling

--- 2-crud-api/src/index.ts
+++ 2-crud-api/src/index.ts
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
 dotenv.config();
-import express, { Express } from 'express';
+import express, { Express, Request, Response, NextFunction } from 'express';
 
 // import { pgInit } from './loaders/pgInit';
 import { userRouter } from './controllers/userRouter';
@@ -14,7 +14,7 @@ async function start() {
   const port = process.env.PORT;
   app.use(express.json());
 
-  app.use((req, res, next) => {
+  app.use((req: Request, res: Response, next: NextFunction) => {
     const { method, url, query, params, body } = req;
     logger.info({ 
       method,
@@ -29,9 +29,31 @@ async function start() {
   app.use('/user/', userRouter);
   app.use('/group/', groupRouter);
 
+  app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    if (res.headersSent) {
+      return next(err);
+    }
+    if (err) {
+      res.status(500);
+      res.send('Internal Server Error');
+    }
+  });
+
   app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
   });
 }
 
 start();
+
+
+process.on('uncaughtException', (error, origin) => {
+  console.log(error);
+  console.log(origin);
+  logger.error({ error, origin });
+  process.exit(1);
+});
+
+process.on('unhandledRejection', (error, origin) => {
+  logger.error({ error, origin });
+});
